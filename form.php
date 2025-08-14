9d1cab92fa90cbcf20f051de3969cbb0f2edb485
Athar2022 <athar.hamady@gmail.com>
2025-08-15 00:08:04 +0300

    Created form.php with POST method

---
+++ form.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User Form</title>
+</head>
+<body>
+    <form method="POST">
+        <label for="username">Username:</label>
+        <input type="text" id="username" name="username" required>
+        
+        <label for="color">Favorite Color:</label>
+        <input type="text" id="color" name="color" required>
+        
+        <button type="submit">Submit</button>
+    </form>
+</body>
+</html>
