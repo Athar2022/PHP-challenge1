67e8120dfca9a901dbd1cca460b0128b445683df
Athar2022 <athar.hamady@gmail.com>
2025-08-15 00:01:43 +0300

    Implemented server time

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+date_default_timezone_set('Asia/Aden');
+$currentTime = date('Y-m-d H:i:s');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Challenge 1</title>
+</head>
+<body>
+    <p>Current server time in Yemen: <?php echo $currentTime; ?></p>
+</body>
+</html>
